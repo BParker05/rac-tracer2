3456a87c71d6226e4c327a0f2dd8c64fedb362e6
Brayden <braydenparker2701@gmail.com>
2026-04-04 19:43:38 +1100

    Chapter 4 done

--- src/main.cpp
+++ src/main.cpp
@@ -18,28 +18,38 @@ Projectile tick(Environment env, Projectile proj){
 
 int main(){
 
-    Tuple p = Point(2,3,4);
+    Canvas c = Canvas(100,100);
+    Colour w = Colour(1,1,1);
+    float r = 35;
 
-    Matrix t1 = shearing(1,0,0,0,0,0);
-    Matrix t2 = shearing(0,1,0,0,0,0);
-    Matrix t3 = shearing(0,0,1,0,0,0);
-    Matrix t4 = shearing(0,0,0,1,0,0);
-    Matrix t5 = shearing(0,0,0,0,1,0);
-    Matrix t6 = shearing(0,0,0,0,0,1);
+    Tuple twelve = Point(0,1,0);
+    Tuple eleven = rotationZ(11 * PI/6) * twelve;
+    Tuple ten = rotationZ(10 * PI/6) * twelve;
+    Tuple nine = rotationZ(9 * PI/6) * twelve;
+    Tuple eight = rotationZ(8 * PI/6) * twelve;
+    Tuple seven = rotationZ(7 * PI/6) * twelve;
+    Tuple six = rotationZ(6 * PI/6) * twelve;
+    Tuple five = rotationZ(5 * PI/6) * twelve;
+    Tuple four = rotationZ(4 * PI/6) * twelve;
+    Tuple three = rotationZ(3 * PI/6) * twelve;
+    Tuple two = rotationZ(2 * PI/6) * twelve;
+    Tuple one = rotationZ(PI/6) * twelve;
 
-    Tuple tp1 = t1*p;
-    Tuple tp2 = t2*p;
-    Tuple tp3 = t3*p;
-    Tuple tp4 = t4*p;
-    Tuple tp5 = t5*p;
-    Tuple tp6 = t6*p;
 
-    std::cout << tp1.x << " " << tp1.y << " " << tp1.z << " " << tp1.w << std::endl;
-    std::cout << tp2.x << " " << tp2.y << " " << tp2.z << " " << tp2.w << std::endl;
-    std::cout << tp3.x << " " << tp3.y << " " << tp3.z << " " << tp3.w << std::endl;
-    std::cout << tp4.x << " " << tp4.y << " " << tp4.z << " " << tp4.w << std::endl;
-    std::cout << tp5.x << " " << tp5.y << " " << tp5.z << " " << tp5.w << std::endl;
-    std::cout << tp6.x << " " << tp6.y << " " << tp6.z << " " << tp6.w << std::endl;
+    c.writePixel(twelve.x * r + 50, twelve.y * r + 50, w);
+    c.writePixel(eleven.x * r + 50, eleven.y * r + 50, w);
+    c.writePixel(ten.x * r + 50, ten.y * r + 50, w);
+    c.writePixel(nine.x * r + 50, nine.y * r + 50, w);
+    c.writePixel(eight.x * r + 50, eight.y * r + 50, w);
+    c.writePixel(seven.x * r + 50, seven.y * r + 50, w);
+    c.writePixel(six.x * r + 50, six.y * r + 50, w);
+    c.writePixel(five.x * r + 50, five.y * r + 50, w);
+    c.writePixel(four.x * r + 50, four.y * r + 50, w);
+    c.writePixel(three.x * r + 50, three.y * r + 50, w);
+    c.writePixel(two.x * r + 50, two.y * r + 50, w);
+    c.writePixel(one.x * r + 50, one.y * r + 50, w);
+
+    c.writePPM();
     
     return 0; 
 }
