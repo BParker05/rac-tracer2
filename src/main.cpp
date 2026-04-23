7a32d30909fba93cec458ec5804da4ee7a1635a2
Brayden <braydenparker2701@gmail.com>
2026-04-23 19:48:26 +1000

    Chapter 5 done

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,36 @@ Projectile tick(Environment env, Projectile proj){
 
 int main(){
 
-    Ray r = Ray(Point(0,0,-5), Vector(0,0,1));
-    Sphere s = Sphere(0);
-    s.setTransform(translation(5,0,0));
-    std::vector<Intersection> xs = intersect(r, s);
+    Tuple rayOrigin = Point(0,0,-5);
+    float wallZ = 10;
+    float wallSize = 7;
+    float canvasPixels = 200;
+    float pixelSize = wallSize / canvasPixels;
+    float half = wallSize / 2;
 
-    std::cout << xs.size() << std::endl;
-    std::cout << xs[0].t << std::endl;
-    std::cout << xs[1].t << std::endl;
+    Canvas c = Canvas(canvasPixels, canvasPixels);
+    Colour red = Colour(1,0,0);
+    Sphere s = Sphere(0);
+
+    for (int i = 0; i < canvasPixels; i++){
+        float worldY = half - pixelSize * i;
+
+        for (int j = 0; j < canvasPixels; j++){
+            float worldX = -half + pixelSize * j;
+
+            Tuple position = Point(worldX, worldY, wallZ);
+            Ray r = Ray(rayOrigin, normalize(position - rayOrigin));
+            std::vector<Intersection> xs = intersect(r,s);
+
+            if (xs.size() > 0){
+                c.writePixel(j,i,Colour(j/200.0,i/200.0,j/200.0));
+            } else {
+                c.writePixel(j,i,Colour(0,0,0));
+            }
+        }
+    }
+
+    c.writePPM();
 
     return 0; 
 }
